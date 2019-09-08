d8ebf6810b55ce14bdeb0a4db11c1b7f2719dde0
milanpanin <milanpanin@gmail.com>
2019-09-08 20:17:27 +0200

    Adding new branch

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
-let heart = document.getElementById("heart");
-let num_likes = document.getElementById("likes");
+var browserWidth = window.innerWidth;
+
+if (browserWidth > 650) {
+    var heart = document.getElementById("heart");
+    var numLikes = document.getElementById("likes");
+} else {
+    var heart = document.getElementById("heart_phone");
+    var numLikes = document.getElementById("likes_phone");
+}
 
 heart.addEventListener("click", () => {
-    if(num_likes.textContent == 1){
-        num_likes.textContent = 0;
-        heart.classList.add('far'); 
+    if (numLikes.textContent == 1) {
+        numLikes.textContent = 0;
+        heart.classList.add('far');
         heart.classList.remove('fas');
-    }
-    else if(num_likes.textContent == 0){
-        num_likes.textContent = 1;
+    } else if (numLikes.textContent == 0) {
+        numLikes.textContent = 1;
         heart.classList.add('fas');
         heart.classList.remove('far');
     }
